b3965cdbc3978bae1ecc52afd4746229b78a3bb2
User <User@DESKTOP-0BJFLEG>
2021-02-01 00:25:50 +0200

    Add project files.

---
+++ HWLesson23/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson23
+{
+    public class Customer : IComparable<Customer>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public string Address { get; set; }
+        public int Protection { get; set; }
+        public int TotalPurchases { get; set; }
+
+        public Customer(int id, string name, int birthYear, string address, int protection)
+        {
+            Id = id;
+            Name = name;
+            BirthYear = birthYear;
+            Address = address;
+            Protection = protection;
+        }
+
+        public override string ToString()
+        {
+            return $"\nID: {Id, 5}   Name: {Name,10}    Birth Year: {BirthYear, 4}" +
+                $"\n Address: {BirthYear} \n Protection: {Protection} \n Total Purchases: {TotalPurchases}";
+        }
+
+        public int CompareTo(Customer other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class CustomerCompareByProtection : IComparer<Customer>
+        {
+            public int Compare(Customer x, Customer y)
+            {
+                return y.Protection - x.Protection;
+            }
+        }
+
+        public class CustomerCompareByTotalPurchases : IComparer<Customer>
+        {
+            public int Compare(Customer x, Customer y)
+            {
+                return y.TotalPurchases - x.TotalPurchases;
+            }
+        }
+
+        public class CustomerCompareByBirthYear : IComparer<Customer>
+        {
+            public int Compare(Customer x, Customer y)
+            {
+                return y.BirthYear - x.BirthYear;
+            }
+        }
+    }
+}
---
+++ HWLesson23/MyQueue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson23
+{
+    class MyQueue
+    {
+        private List<Customer> customers = new List<Customer>();
+
+        public void Enqeueue(Customer c) {
+            customers.Add(c);
+        }
+
+        public Customer Dequeue() {
+            if (customers.Count > 0)
+            {
+                Customer result = customers[0];
+                customers.RemoveAt(0);
+                return result;
+            }
+            return null;
+        }
+
+        public void Init(List<Customer> newList) 
+        { 
+            this.customers = newList;
+        }
+
+        public void Clear()
+        {
+            customers.Clear();
+        }
+        public Customer WhoIsNext() {
+            if (customers.Count == 0)
+                return null;
+
+            return customers[0];
+        }
+        public int Count() 
+        {
+            return customers.Count;
+        }
+        public void SortByProtection() 
+        { 
+            customers.Sort(new Customer.CustomerCompareByProtection());
+        }
+        public void SortByTotalPurchases() 
+        {
+            customers.Sort(new Customer.CustomerCompareByTotalPurchases());
+        }
+        public void SortByBirthYear() 
+        {
+            customers.Sort(new Customer.CustomerCompareByBirthYear()); 
+        }
+        public List<Customer> DequeueCustomer(int x)
+        {
+            if (x > customers.Count)
+            {
+                Console.WriteLine("Errore");
+            }
+
+            List<Customer> result = new List<Customer>();
+            while (customers.Count > 0 && result.Count < x)
+            {
+                result.Add(customers[0]);
+                customers.RemoveAt(0);
+            }
+
+            return result;
+        }
+        public void AniRakSheela(Customer c) 
+        { 
+            customers.Insert(0, c);
+        }
+      //  public Customer DequeueProtectzia() { }
+
+    }
+}
